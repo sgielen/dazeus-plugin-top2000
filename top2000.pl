136a31b0a81b2555bfea8cfbd07eafc547ef95f4
Sjors Gielen <sjors@sjorsgielen.nl>
2014-12-30 19:14:38 +0100

    Move while loop above method and wait until the current track is almost expired before rerequesting

--- top2000.pl
+++ top2000.pl
@@ -13,6 +13,29 @@ if(!$channel) {
 my $json_url = "http://top2012.radio2.nl/data/cache/json/nowplaying.json";
 my $last_song_id;
 
+while(1) {
+	my $song = get_new_song();
+	if(!$song) {
+		sleep 5;
+		next;
+	}
+
+	my $message = "TOP 2000 - Just started: " . $song->{'artist'} . " - " . $song->{'title'} . " - Listen in: http://radioplayer.npo.nl/radio2/";
+	print $message . "\n";
+	try {
+		my $dazeus = DaZeus->connect($sock);
+		$dazeus->message($network, $channel, $message);
+	} catch {
+		print "Failed to deliver message: $_\n";
+	};
+
+	my $time_left = $song->{'expires'} - time;
+	if($time_left > 6) {
+		print "Sleeping $time_left - 6\n";
+		sleep $time_left - 6;
+	}
+}
+
 sub get_new_song {
 	my $body = `wget -qO- "$json_url"`;
 	my $obj = decode_json($body);
@@ -25,17 +48,3 @@ sub get_new_song {
 	}
 }
 
-while(1) {
-	my $song = get_new_song();
-	if($song) {
-		my $message = "TOP 2000 - Just started: " . $song->{'artist'} . " - " . $song->{'title'} . " - Listen in: http://radioplayer.npo.nl/radio2/";
-		print $message . "\n";
-		try {
-			my $dazeus = DaZeus->connect($sock);
-			$dazeus->message($network, $channel, $message);
-		} catch {
-			print "Failed to deliver message: $_\n";
-		};
-	}
-	sleep 5;
-}
